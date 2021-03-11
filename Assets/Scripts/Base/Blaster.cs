54546a6f0a7eb7c11395c243a467481f7cd6d86f
jdaev <jdhn42@gmail.com>
2021-03-11 15:59:00 -0500

    feat: Added color switching and color indicator to blaster
    Fire different bullets based on selection

--- Assets/Scripts/Base/Blaster.cs
+++ Assets/Scripts/Base/Blaster.cs
@@ -1,29 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using Base;
+using Managers;
 using UnityEngine;
 
 public class Blaster : MonoBehaviour
 {
+    [SerializeField] private GameObject nozzle;
+    [SerializeField] private GameObject colorIndicator;
+
+    private int _activeColorIndex = 0;
+    private STLColor[] _colors = new[] {STLColor.Red, STLColor.Blue, STLColor.Green};
+
     private bool _isFiring;
 
+    private Material _colorIndicatorMaterial;
     public void Initialize()
     {
-        
+        _colorIndicatorMaterial = colorIndicator.GetComponent<MeshRenderer>().material;
+        SetIndicatorColor();
     }
-
+    
     public void Refresh()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SwitchColor(-1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SwitchColor(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Fire();
+        }
     }
-    
+
     private void Fire()
     {
+        BulletManager.Instance.ShootBullet(_colors[_activeColorIndex], nozzle.transform.position);
+    }
+
+    private void SwitchColor(int axis)
+    {
+        if (axis < 0)
+        {
+            _activeColorIndex = _activeColorIndex == 0 ? _colors.Length - 1 : _activeColorIndex - 1;
+        }
+        else
+        {
+            _activeColorIndex = _activeColorIndex == (_colors.Length - 1) ? 0 : _activeColorIndex + 1;
+        }
+        SetIndicatorColor();
         
     }
 
-    private void SwitchColor()
+    private void SetIndicatorColor()
     {
-        
+        _colorIndicatorMaterial.color = Values.colorMap[_colors[_activeColorIndex]];
     }
-    
 }
