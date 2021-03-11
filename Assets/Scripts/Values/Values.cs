e4008c667b8271d21bd704f285a0f2d7d4f22a53
jdaev <jdhn42@gmail.com>
2021-03-11 15:57:14 -0500

    task: Added Values static class to store constants

--- Assets/Scripts/Values/Values.cs
+++ Assets/Scripts/Values/Values.cs
@@ -1,7 +1,14 @@
-﻿namespace Values
-{
+﻿using System.Collections.Generic;
+using Base;
+using UnityEngine;
+
+
     public static class Values
     {
-        
+        public static Dictionary<STLColor, Color> colorMap = new Dictionary<STLColor, Color>()
+        {
+            {STLColor.Red, Color.red},
+            {STLColor.Blue, Color.blue},
+            {STLColor.Green, Color.green},
+        };
     }
-}
