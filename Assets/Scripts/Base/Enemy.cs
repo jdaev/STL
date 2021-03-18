10f9a6d894a8a17715d9df6697c0619569d5edb5
jdaev <jdhn42@gmail.com>
2021-03-18 19:00:36 -0400

    fix: Fixed enemy not being set the right spawn animation

--- Assets/Scripts/Base/Enemy.cs
+++ Assets/Scripts/Base/Enemy.cs
@@ -8,13 +8,16 @@ namespace Base
     public class Enemy : MonoBehaviour, IPoolable
     {
         public ShootableColor color;
-
+        public SpawnPosition spawnPosition;
         private Animator _animator;
         private float speed = 10f;
-        
+        private static readonly int SpawnPosition = Animator.StringToHash("SpawnPosition");
+
         public void Initialize()
         {
             _animator = gameObject.GetComponent<Animator>();
+            int i = (int) spawnPosition;
+            _animator.SetInteger(SpawnPosition, (int) spawnPosition);
         }
         bool AnimatorIsPlaying(){
             return _animator.GetCurrentAnimatorStateInfo(0).length >
@@ -28,7 +31,7 @@ namespace Base
 
         private void Move()
         {
-            transform. position= Vector3.MoveTowards(transform.position,
+            transform.position= Vector3.MoveTowards(transform.position,
                 GameManager.Instance.PlayerManager.Player.transform.position, speed * Time.deltaTime);
         }
 
@@ -36,7 +39,7 @@ namespace Base
         {
             if (color.weaknesses.Contains(bulletColor))
             {
-                gameObject.SetActive(false);
+                Kill();
             }
         }
 
