5a16ed24881595735e111a4bb0152250e32564b0
jdaev <jdhn42@gmail.com>
2021-03-19 17:28:02 -0400

    feat: Created enemy spawner manager

---
+++ Assets/Scripts/Managers/EnemySpawnerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Base;
+
+namespace Managers
+{
+    public class EnemySpawnerManager
+    {
+        private static EnemySpawnerManager _instance;
+
+        public static EnemySpawnerManager Instance => _instance ??= new EnemySpawnerManager();
+        private readonly Queue<EnemySpawner> _enemySpawners;
+        
+
+        private EnemySpawnerManager()
+        {
+        }
+
+        public void Initialize()
+        {
+        }
+
+        public void Refresh()
+        {
+            
+        }
+    }
+}
