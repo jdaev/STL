e79b8ba61a080efe2992dc4e60c28e5d636bbdab
jdaev <jdhn42@gmail.com>
2021-04-01 17:26:02 -0400

    feat: UI Manager
    Enable and disable panels
    Update HUD

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,4 +1,7 @@
-﻿namespace Managers
+﻿using TMPro;
+using UnityEngine;
+
+namespace Managers
 {
     public class UIManager
     {
@@ -9,11 +12,39 @@
 
         #endregion
 
-        public void Initialize()
+        private GameObject _gameOverMenu;
+        private GameObject _pauseMenu;
+        
+        private TextMeshProUGUI _scoreText;
+        private TextMeshProUGUI _hitText;
+
+        
+        public void Initialize(GameObject gameOverMenu,GameObject pauseMenu, TextMeshProUGUI scoreText, TextMeshProUGUI hitText)
         {
-            
+            this._gameOverMenu = gameOverMenu;
+            this._pauseMenu = pauseMenu;
+
+            _scoreText = scoreText;
+            _hitText = hitText;
+
         }
 
+        public void OnGameOver()
+        {
+            _pauseMenu.SetActive(false);
+            _gameOverMenu.SetActive(true);
+        }
+
+        public void UpdateHUD(string score, string hit)
+        {
+            _scoreText.text = score;
+            _hitText.text = hit;
+        }
+
+        public void OnPause() => _pauseMenu.SetActive(true);
+
+        public void OnResume() => _pauseMenu.SetActive(false);
+        
         public void Refresh()
         {
             
