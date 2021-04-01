02e5ebfbfad91c1eeb85c8f10ce1e0a5ce1c35a8
jdaev <jdhn42@gmail.com>
2021-04-01 17:24:46 -0400

    refactor: Rename Scene in function

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -66,7 +66,7 @@ namespace Managers
 
         public void ReloadScene()
         {
-            SceneManager.LoadScene("TestScene");
+            SceneManager.LoadScene("GameScene");
         }
     }
 }
