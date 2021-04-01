0f5ca8f0a27de0e6b168111b63a7eb24dae0f82f
jdaev <jdhn42@gmail.com>
2021-04-01 17:22:27 -0400

    feat: Detect menu button press for pause input, toggle interactors

--- Assets/Scripts/Managers/ControllerManager.cs
+++ Assets/Scripts/Managers/ControllerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
+using UnityEngine.InputSystem;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using InputDevice = UnityEngine.InputSystem.InputDevice;
@@ -24,18 +25,22 @@ namespace Managers
         private ActionBasedController _rightHandController;
         private ActionBasedController _leftHandController;
 
+        public InputActionReference PauseInputAction;
+
         public bool IsRightTriggerPressed() => _rightHandController.activateAction.action.triggered;
         public bool IsRightGripPressed() => _rightHandController.selectAction.action.triggered;
-        
+
         public Vector2 RightThumbstickAxis() => _rightHandController.rotateAnchorAction.action.ReadValue<Vector2>();
-        
+
         public bool IsLeftTriggerPressed() => _leftHandController.activateAction.action.triggered;
         public bool IsLeftGripPressed() => _leftHandController.selectAction.action.triggered;
-        
+
+
         public Vector2 LeftThumbstickAxis() => _leftHandController.rotateAnchorAction.action.ReadValue<Vector2>();
 
 
-        public void Initialize(GameObject leftHandController, GameObject rightHandController, GameObject headset)
+        public void Initialize(GameObject leftHandController, GameObject rightHandController, GameObject headset,
+            InputActionReference pauseInputAction)
         {
             this._leftHandGameObject = leftHandController;
             this._rightHandGameObject = rightHandController;
@@ -43,6 +48,37 @@ namespace Managers
 
             _rightHandController = _rightHandGameObject.GetComponent<ActionBasedController>();
             _leftHandController = _leftHandGameObject.GetComponent<ActionBasedController>();
+
+            PauseInputAction = pauseInputAction;
+            PauseInputAction.asset.Enable();
+        }
+
+        public void ToggleInteractors(bool toggle)
+        {
+            ToggleLeftHandInteractors(toggle);
+            ToggleRightHandInteractors(toggle);
+        }
+
+        private void ToggleLeftHandInteractors(bool toggle)
+        {
+            XRRayInteractor xrRayInteractor = _leftHandGameObject.GetComponent<XRRayInteractor>();
+            LineRenderer lineRenderer = _leftHandGameObject.GetComponent<LineRenderer>();
+            XRInteractorLineVisual xrInteractorLineVisual = _leftHandController.GetComponent<XRInteractorLineVisual>();
+
+            xrRayInteractor.enabled = toggle;
+            lineRenderer.enabled = toggle;
+            xrInteractorLineVisual.enabled = toggle;
+        }
+        
+        private void ToggleRightHandInteractors(bool toggle)
+        {
+            XRRayInteractor xrRayInteractor = _rightHandGameObject.GetComponent<XRRayInteractor>();
+            LineRenderer lineRenderer = _rightHandGameObject.GetComponent<LineRenderer>();
+            XRInteractorLineVisual xrInteractorLineVisual = _rightHandController.GetComponent<XRInteractorLineVisual>();
+
+            xrRayInteractor.enabled = toggle;
+            lineRenderer.enabled = toggle;
+            xrInteractorLineVisual.enabled = toggle;
         }
     }
 }
