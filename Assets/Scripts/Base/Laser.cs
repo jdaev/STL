d192e19224c7ae24ae05e0ce05671f84c75b5304
jdaev <jdhn42@gmail.com>
2021-04-03 18:44:18 -0400

    feat: laser class and prefabs

---
+++ Assets/Scripts/Base/Laser.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using Managers;
+using UnityEngine;
+
+namespace Base
+{
+    public class Laser : MonoBehaviour, IPoolable
+    {
+        private LineRenderer _lineRenderer;
+        private float lifetime = 0.75f;
+
+        private Transform _origin;
+        private Vector3 _destination;
+        
+        public STLColor color;
+
+
+        public virtual void Initialize(Transform origin, Vector3 destination)
+        {
+            if (_lineRenderer == null)
+            {
+                _lineRenderer = GetComponent<LineRenderer>();
+            }
+
+            _origin = origin;
+            _destination = destination;
+            
+            
+            transform.position = _origin.position;
+            transform.rotation = _origin.rotation;
+
+
+            _lineRenderer.SetPosition(0, transform.position);
+            _lineRenderer.SetPosition(1, _destination);
+
+            StartCoroutine(Destroy());
+        }
+
+        IEnumerator Destroy()
+        {
+            yield return new WaitForSeconds(lifetime);
+
+            GameManager.Instance.LaserManager.RemoveLaser(this);
+        }
+
+        public void Refresh()
+        {
+            //transform.Translate(transform.forward * (GameManager.Instance.Level.playerSpeed * Time.deltaTime));
+        }
+
+
+        public void Pooled()
+        {
+        }
+
+        public void DePooled()
+        {
+        }
+
+        public GameObject GetGameObject => gameObject;
+    }
+}
